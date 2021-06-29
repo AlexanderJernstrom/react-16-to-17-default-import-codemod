dfe9242e32720144f0a25a49c0db9b7e84bb7948
Alexander Jernström <46424392+AlexanderJernstrom@users.noreply.github.com>
2021-06-29 16:54:30 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { API, FileInfo } from "jscodeshift";
+
+const log = (p) => {
+  console.log(p.node);
+  return true;
+};
+
+module.exports = function (fileInfo: FileInfo, api: API, options) {
+  const root = api.jscodeshift(fileInfo.source);
+  const j = api.jscodeshift;
+  root
+    .find(j.ImportDeclaration, { source: { value: "react" } })
+    .replaceWith((p) => {
+      const hasDefaultImportReact = p.node?.specifiers?.some(
+        (specifier) => specifier.type === "ImportDefaultSpecifier"
+      );
+      if (!hasDefaultImportReact) return p.node;
+      const specifiersWithoutReact = p.node?.specifiers?.filter(
+        (specifier) => specifier.type !== "ImportDefaultSpecifier"
+      );
+      if (specifiersWithoutReact.length === 0) return null;
+      return { ...p.node, specifiers: specifiersWithoutReact };
+    });
+
+  return root.toSource();
+};
